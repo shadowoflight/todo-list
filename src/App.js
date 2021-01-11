ffb7974817cbaa31cc07a3ae47f5f42e199d9ead
Sanjana Narayan <ni8fury@Sanjanas-MacBook-Pro.local>
2021-01-11 20:06:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TodoList from './TodoList.js';
+import './table.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    todos: [],
+    pending:0
+};
+  }
+
+deleteItem = (toBeDel) => {
+  const filteredItems = this.state.todos.filter(x => x.Id !== toBeDel.Id);
+  this.setState({
+       todos: filteredItems
+  });
+};
+editStats = (stats) => {
+  this.setState(state => ({
+       todos: state.todos.map(todo => {
+         if (todo.Id === stats.Id) {
+             return {
+                   ...todo,
+                   status: todo.Status === "Done" ? "Pending" : "Done"
+             };
+        } else {
+            return todo;
+        }
+    })
+}));
+};
+addToDo = (todo) => {
+  this.setState({
+      todos: [...this.state.todos, todo]
+  });
+};
+
+render() {
+    return (
+         <div className="container" >
+             <h1>TodoList </h1>
+             <TodoList onAdd={this.addToDo} />
+             <h3>You have {this.state.todos.length} item in your list</h3>
+             <table className="item-table">
+                   <thead>
+                     <tr>
+                        <th>Sno.</th>
+                        <th>Name of Task</th>
+                        <th>Status</th>
+                        <th></th>
+                        <th></th>
+                        <th>Action</th>
+                        
+                     </tr>
+                   </thead>
+                   <tbody>
+                       {this.state.todos.map(x => {
+                         return (
+                              <tr key={x.Id}>
+                              <td>{x.Id}</td>
+                              <td>{x.Title}</td>
+                              <td>{x.status}</td>
+                              <td></td>
+                              
+                              <td>
+                                 <button onClick={() => this.deleteItem(x) }>Delete</button>
+                              </td>
+                              <td><button onClick={() => this.editStats(x)}>Edit Status</button></td>
+                              </tr>
+                          );
+                       })}
+                    </tbody>
+                </table>
+          </div> 
+      );
+   }
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import './table.css'
+export default class TodoList extends Component {
+    constructor(props){
+        super(props)
+    this.state = {
+        Id: "",
+        Title: "",
+        status: "Pending"
+
+    };
+}
+   handleIdChange = (event) => {
+        this.setState({
+            Id: event.target.value
+        });
+    };
+    handleTitleChange = (event) => {
+        this.setState({
+            Title: event.target.value
+        });
+    }
+    handleStatusChange = (event) => {
+        this.setState({
+            status: event.target.value
+        });
+    };
+    handleToDoSubmit = (event) => {
+        event.preventDefault();
+        this.props.onAdd({
+            Id: this.state.Id,
+            Title: this.state.Title,
+            status: this.state.status
+        });
+        this.setState({
+            Id: "",
+            Title: "",
+            status: "Pending"
+        });
+    };
+    componentDidMount(){
+    }
+    render() {
+        return (
+            <div>
+                <h3>
+                    Add ToDo
+                </h3>
+                <form className="forms" onSubmit={this.handleToDoSubmit} >
+                    <div className="form-group" >
+                        <label>Id: </label>
+                        <input value={this.state.Id} onChange={this.handleIdChange} className="form-control" placeholder="Enter Id" />
+                    </div>
+                    <div className="form-group" >
+                    <label>Item Name: </label>
+                        <input value={this.state.Title} onChange={this.handleTitleChange} className="form-control" placeholder="Enter Title" />
+                    </div>
+                    <div className="form-group">
+                        <select value={this.state.status} onChange={this.handleStatusChange} className="form-control" >
+                            <option value="Done" >Done</option>
+                            <option value="Pending" >Pending</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="form-control btn btn-primary" >Add Todo</button>
+                </form>
+            </div>
+        );
+    }
+}
